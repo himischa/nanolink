5c18bb21fe8700482a73ce171ebffd7e0672cc18
himischa <mischa.workspace@gmail.com>
2026-05-19 18:43:52 +0800

    feat: added QRService

---
+++ src/main/java/com/nanolink/qr/QrService.java
@@ -0,0 +1,30 @@
+package com.nanolink.qr;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QrService {
+    public byte[] generateQrCode(String url) throws WriterException, IOException {
+        QRCodeWriter writer = new QRCodeWriter();
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+        hints.put(EncodeHintType.MARGIN, 1);
+
+        BitMatrix matrix = writer.encode(url, BarcodeFormat.QR_CODE, 300, 300, hints);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(matrix, "PNG", outputStream);
+        return outputStream.toByteArray();
+    }
+}
